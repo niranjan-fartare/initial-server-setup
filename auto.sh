ae1916fae5caa7deef21b6bc684fdf4213e46770
niranjan-exe <77389312+niranjan-exe@users.noreply.github.com>
2021-04-04 23:11:41 +0530

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -6,9 +6,12 @@ sudo apt autoremove;
 
 sudo apt clean;
 
-sudo apt install apache2 php -y;
+sudo apt install apache2 php unzip -y;
+
 
 sudo apt install php-curl php-gd php-mbstring php-xml php-xmlrpc php-soap php-intl php-zip php-mysql -y;
+
+sudo service apache2 restart;
 
 sudo echo "<Directory /var/www/html/>" | sudo tee -a /etc/apache2/apache.conf;
 sudo echo "    AllowOverride All" | sudo tee -a /etc/apache2/apache.conf;
@@ -17,9 +20,9 @@ sudo echo "</Directory>" | sudo tee -a /etc/apache2/apache.conf;
 sudo apt install curl;
 
 cd /tmp;
-sudo curl -O https://wordpress.org/latest.tar.gz;
+sudo wget https://wordpress.org/latest.zip;
 
-sudo tar xzvf latest.tar.gz;
+sudo unzip latest.zip;
 sudo touch /tmp/wordpress/.htaccess;
 sudo cp /tmp/wordpress/wp-config-sample.php /tmp/wordpress/wp-config.php;
 sudo mkdir /tmp/wordpress/wp-content/upgrade;
@@ -30,6 +33,14 @@ sudo chown -R www-data:www-data /var/www/html;
 
 sudo find /var/www/html/ -type d -exec chmod 750 {} \; ;
 sudo find /var/www/html/ -type f -exec chmod 640 {} \; ;
+
+sudo fallocate -l 2G /swapfile;
+sudo chmod 600 /swapfile;
+sudo mkswap /swapfile;
+sudo swapon /swapfile;
+sudo echo "/swapfile swap swap defaults 0 0" | sudo tee -a /etc/fstab ;
+
+
 
 
 
